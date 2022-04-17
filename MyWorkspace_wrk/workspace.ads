7b00d2043f5ce670ff7d29e9b999a487b02f7a45
dmdars <45941499+dmdars@users.noreply.github.com>
2022-04-17 21:27:02 +0700

    Initial Commit

---
+++ MyWorkspace_wrk/workspace.ads
@@ -0,0 +1,31 @@
+<ADSWorkspace Revision="1" Version="100">
+    <Workspace Name="">
+        <Library Name="ads_standard_layers" />
+        <Library Name="adstechlib" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_verification_test_bench" />
+        <Library Name="MyLibrary_lib" />
+        <Cell Name="MyLibrary_lib:cell_1" />
+        <Log Name="search_history.log" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="MyLibrary_lib_lay.prf" />
+        <Preferences Name="schematic.prf" />
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+    </Workspace>
+</ADSWorkspace>
